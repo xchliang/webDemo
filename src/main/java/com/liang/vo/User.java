f08eef9c68c810f5188806dd2dd6932e3a057fe0
xcl <xclcba@163.com>
2019-07-31 11:20:58 +0800

    vo

---
+++ src/main/java/com/liang/vo/User.java
@@ -0,0 +1,36 @@
+package com.liang.vo;
+
+public class User {
+    public String name;
+    public Integer age;
+    public String address;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

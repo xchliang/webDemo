f7969831ffad951155eafd5987201a745798c6e6
xinch <xclcba@163.com>
2020-06-30 10:23:37 +0800

    增加注释

--- src/main/java/com/liang/design/factory/abstractFactory/AbstractFactoryTest.java
+++ src/main/java/com/liang/design/factory/abstractFactory/AbstractFactoryTest.java
@@ -4,6 +4,7 @@ package com.liang.design.factory.abstractFactory;
  * 抽象工厂模式
  * 抽象工厂：定义创建多种产品的方法
  * 实例工厂：创建一组配套的产品
+ * 与工厂方法模式区别：工厂方法模式工厂只生产一种产品，而不是一组配套的产品
  */
 public class AbstractFactoryTest {
     public static void main(String[] args) {
@@ -11,6 +12,8 @@ public class AbstractFactoryTest {
         PhoneFactory[] phoneFactoryArr = new PhoneFactory[]{new HuaweiFactroy(), new XiaoMiFactory()};
         for (PhoneFactory phoneFactory : phoneFactoryArr) {
             System.out.println("==========工厂创建产品==========开始==");
+            //手机、电池、屏幕是配套产品
+            //创建了小米工厂，就可生产小米手机及配套的电池、屏幕等产品
             IPhone phone = phoneFactory.createPhone();//生产手机
             IBattery battery = phoneFactory.createBattery();//生产电池
             IScreen screen = phoneFactory.createScreen();//生产屏幕
